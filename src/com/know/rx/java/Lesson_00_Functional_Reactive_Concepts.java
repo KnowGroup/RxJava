ec3b48166360b4c1d02f4a8b075ea2eb3cae0a87
KnowGroup <45757509+KnowGroup@users.noreply.github.com>
2019-01-15 14:21:14 +0530

    Initial setup of Reactive extension of Java Project from Know Group

---
+++ src/com/know/rx/java/Lesson_00_Functional_Reactive_Concepts.java
@@ -0,0 +1,38 @@
+package com.know.rx.java;
+
+/**
+ *
+ * @author KnowGroup
+ */
+public class Lesson_00_Functional_Reactive_Concepts {
+
+    /*
+      Pure function always return the same result for a given set of Parameter
+      values.
+    */
+    public double pure_function(int n){
+        return n*n;
+    }
+    /*
+       Impure function are not thread safe results varry.
+    */
+    double state = -1;
+    public double impure_function(int n){
+        state = (++state % 3);
+        return n * 2 + state;
+    }
+    
+    public static void main(String[] arg){
+        Lesson_00_Functional_Reactive_Concepts l = new Lesson_00_Functional_Reactive_Concepts();
+        
+        for(int i = 1;i<10;i++){
+            if(l.pure_function(5) != 25){
+                throw new RuntimeException("Not Pure Function");
+            }
+        }
+        
+        for(int i = 1;i<100;i++){
+            System.out.println("Impure :: "+l.impure_function(5));
+        }
+    }
+}
